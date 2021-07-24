c1cd94c8dad1457c4490a80ef75df98778e1a36d
sju02048 <sju02048@naver.com>
2021-07-25 02:48:28 +0900

    백준solve

---
+++ 백준10828.js
@@ -0,0 +1,41 @@
+var fs = require("fs");
+var input = fs.readFileSync("dev/stdin").toString().trim().split("\n");
+// 계산식
+let stack = [];
+for (i = 1; i <= input[0]; i++) {
+  const command = input[i].split(" ");
+  //push
+  if (command[0] === "push") {
+    stack.push(command[1]);
+  }
+  //pop
+  if (command[0] === "pop") {
+    if (stack.length === 0) {
+      console.log(-1);
+    } else {
+      console.log(stack.pop());
+    }
+  }
+  //size
+  if (command[0] === "size") {
+    console.log(stack.length);
+  }
+  //empty
+  if (command[0] === "empty") {
+    if (stack.length === 0) {
+      console.log(1);
+    } else {
+      console.log(0);
+    }
+  }
+  //top
+  if (command[0] === "top") {
+    if (stack.length === 0) {
+      console.log(-1);
+    } else {
+      console.log(stack[stack.length - 1]);
+    }
+  }
+}
+
+// 시간 초과 이슈 ..
---
+++ 백준10845.js
@@ -0,0 +1,33 @@
+var fs = require("fs");
+var input = fs
+  .readFileSync("dev/stdin")
+  .toString()
+  .trim()
+  .split("\n")
+  .map((a) => a);
+
+// 계산식
+const countInput = +input[0];
+let stack = [];
+let result = "";
+
+for (let i = 1; i <= countInput; i++) {
+  const command = input[i].split(" ");
+
+  if (command[0] === "push") stack.push(input[i][5]);
+  else if (command[0] === "pop")
+    stack.length ? (result += `${stack.shift()}\n`) : (result += `-1\n`);
+  else if (command[0] === "size") result += `${stack.length}\n`;
+  else if (command[0] === "empty")
+    stack.length ? (result += `0\n`) : (result += `1\n`);
+  else if (command[0] === "front")
+    stack.length ? (result += `${stack[0]}\n`) : (result += `-1\n`);
+  else
+    stack.length
+      ? (result += `${stack[stack.length - 1]}\n`)
+      : (result += `-1\n`);
+}
+result = result.trim();
+console.log(result);
+
+// 큐를 구현해서 만들었어야 함.

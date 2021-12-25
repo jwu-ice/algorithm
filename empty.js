2b278786d66f3e216f457d067adc4f52cee2c18b
jwu <sju02048@naver.com>
2021-12-25 17:33:42 +0900

    Update empty.js

--- empty.js
+++ empty.js
@@ -1,7 +1,7 @@
-charCount("aaaa");
+// charCount("aaaa");
 // {a: 4}
 
-charCount("hello");
+// charCount("hello");
 /*
 {
   h: 1,
@@ -11,7 +11,7 @@ charCount("hello");
 }
 */
 
-charCount("Your PIN number is 1234!");
+// charCount("Your PIN number is 1234!");
 /*
   {
     1: 1,
@@ -36,27 +36,95 @@ charCount("Your PIN number is 1234!");
 //   return result;
 // }
 
-function charCount(str) {
-  let obj = {};
-  for (let char of str) {
-    if (isAlphaNumeric(char)) {
-      char = char.toLowerCase();
-      obj[char] = ++obj[char] || 1;
-    }
-  }
-  return obj;
-}
+// function charCount(str) {
+//   let obj = {};
+//   for (let char of str) {
+//     if (isAlphaNumeric(char)) {
+//       char = char.toLowerCase();
+//       obj[char] = ++obj[char] || 1;
+//     }
+//   }
+//   return obj;
+// }
 
-function isAlphaNumeric(char) {
-  const code = char.charCodeAt(0);
-  if (
-    !(code > 47 && code < 58) &&
-    !(code > 64 && code < 91) &&
-    !(code > 96 && code < 123)
-  ) {
-    return false;
-  }
-  return true;
-}
+// function isAlphaNumeric(char) {
+//   const code = char.charCodeAt(0);
+//   if (
+// !(code > 47 && code < 58) &&
+// !(code > 64 && code < 91) &&
+// !(code > 96 && code < 123)
+//   ) {
+//     return false;
+//   }
+//   return true;
+// }
 
-console.log(charCount("Hello! My name is Jwu"));
+// console.log(charCount("Hello! My name is Jwu"));
+
+// 빈도수 세기 패턴
+// 내 답
+// function validAnagram(one, two) {
+//   const obj = {};
+//   for (const val of one) {
+//     obj[val] = (obj[val] || 0) + 1;
+//   }
+
+//   for (const val of two) {
+//     obj[val] === 1 ? delete obj[val] : obj[val]--;
+//   }
+
+//   for (const val in obj) {
+//     if (obj.hasOwnProperty(val)) {
+//       return false;
+//     }
+//   }
+//   return true;
+// }
+
+// validAnagram("awesome", "awesom");
+
+// 콜트샘 답
+// function validAnagram(one, two) {
+//   if (one.length !== two.length) {
+//     return false;
+//   }
+
+//   const obj = {};
+
+//   for (const val of one) {
+//     obj[val] = (obj[val] || 0) + 1;
+//   }
+
+//   for (const val of two) {
+//     if (!obj[val]) {
+//       return false;
+//     }
+//     obj[val]--;
+//   }
+
+//   return true;
+// }
+// validAnagram("awesome", "awesom");
+
+// 다중 포인터 패턴
+// 내 답
+// function countUniqueValues(arr) {
+//   if (arr.length === 0) {
+//     return 0;
+//   }
+
+//   let left = 0;
+//   for (let i = 1; i < arr.length; i++) {
+//     if (arr[left] < arr[i]) {
+//       left++;
+//       arr[left] = arr[i];
+//     }
+//   }
+
+//   return left + 1;
+// }
+
+// console.log(countUniqueValues([1, 2, 2, 2, 2, 3]));
+
+
+

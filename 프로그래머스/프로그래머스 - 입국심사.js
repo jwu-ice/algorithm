7aa1d65329d9a46180c275b6fff0232b8d832424
jwu <sju02048@naver.com>
2021-12-31 03:14:47 +0900

    ♻️ mid 변수 상위로 이동

--- 프로그래머스/프로그래머스 - 입국심사.js
+++ 프로그래머스/프로그래머스 - 입국심사.js
@@ -1,16 +1,15 @@
 function solution(n, times) {
   let left = 1;
   let right = n * Math.max(...times);
-
+  let mid;
   while (left < right) {
-    let mid = Math.floor((left + right) / 2);
+    mid = Math.floor((left + right) / 2);
     let count = times.reduce((prev, curr) => prev + Math.floor(mid / curr), 0);
 
     console.log("left,mid,right,count :>> ", left, mid, right, count);
     count >= n ? (right = mid) : (left = mid + 1);
   }
-
   return left;
 }
 
-solution(6, [2, 10]);
+solution(6, [7, 10]);

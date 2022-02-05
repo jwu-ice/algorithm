ea826dbefd4d7dcd1717be3deb59e8b68bcc1729
sju02048 <sju02048@naver.com>
2022-02-05 18:40:40 +0900

    Update

---
+++ 백준/0_inputform.js
@@ -0,0 +1,11 @@
+const input = require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .split(" ")
+  .map((v) => v.trim());
+
+solution(`3 2`.split(" ").map((v) => v.trim()));
+
+function solution(input) {
+  console.log(input[0] - input[1]);
+}

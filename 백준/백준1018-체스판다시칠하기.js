bcbc4f51f8f114b3b06c4906757548517cffedda
sju02048 <sju02048@naver.com>
2022-04-19 03:38:45 +0900

    Create 백준1018-체스판다시칠하기.js

---
+++ 백준/백준1018-체스판다시칠하기.js
@@ -0,0 +1,80 @@
+// const input = require("fs")
+//   .readFileSync("/dev/stdin")
+//   .toString()
+//   .trim()
+//   .split("\n");
+
+const input = `8 8
+WBWBWBWB
+BWBWBWBW
+WBWBWBWB
+BWBBBWBW
+WBWBWBWB
+BWBWBWBW
+WBWBWBWB
+BWBWBWBW`
+  .toString()
+  .trim()
+  .split("\n");
+
+const [N, M] = input.shift().split(" ");
+const paintArr = [];
+
+const whiteFirstBoard = [
+  "WBWBWBWB",
+  "BWBWBWBW",
+  "WBWBWBWB",
+  "BWBWBWBW",
+  "WBWBWBWB",
+  "BWBWBWBW",
+  "WBWBWBWB",
+  "BWBWBWBW",
+];
+
+const blackFirstBoard = [
+  "BWBWBWBW",
+  "WBWBWBWB",
+  "BWBWBWBW",
+  "WBWBWBWB",
+  "BWBWBWBW",
+  "WBWBWBWB",
+  "BWBWBWBW",
+  "WBWBWBWB",
+];
+
+function paintWhiteFirst(x, y) {
+  let count = 0;
+
+  for (let i = y; i < y + 8; i++) {
+    for (let j = x; j < x + 8; j++) {
+      if (input[i][j] !== whiteFirstBoard[i - y][j - x]) {
+        count++;
+      }
+    }
+  }
+
+  return count;
+}
+
+function paintBlackFirst(x, y) {
+  let count = 0;
+
+  for (let i = y; i < y + 8; i++) {
+    for (let j = x; j < x + 8; j++) {
+      if (input[i][j] !== blackFirstBoard[i - y][j - x]) {
+        count++;
+      }
+    }
+  }
+
+  return count;
+}
+
+for (let i = 0; i + 7 < N; i++) {
+  for (let j = 0; j + 7 < M; j++) {
+    paintArr.push(paintWhiteFirst(j, i));
+    paintArr.push(paintBlackFirst(j, i));
+  }
+}
+
+console.log(Math.min(...paintArr));

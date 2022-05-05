e0a88dfb9f3e59309d7ae723b72eaae51c1cee8b
sju02048 <sju02048@naver.com>
2022-05-05 23:56:39 +0900

    Update TEST.js

--- TEST.js
+++ TEST.js
@@ -1,29 +1,24 @@
-// 머지소트
-const arr = [5, 2, 8, 4, 1, 10, 7, 3, 9, 6];
+function solution(v) {
+  const data = v.join(",").split(",");
+  console.log(data);
+  const result = data
+    .reduce((prev, curr, index) => {
+      if (prev.includes(curr)) {
+        const indexof = prev.indexOf(curr);
+        prev.splice(indexof, 1);
+      } else {
+        prev.push(curr);
+      }
+      return prev;
+    }, [])
+    .map(Number);
 
-function merge(left, right) {
-  const sortedArr = [];
-  console.log("left,right:", left, right);
-  while (left.length && right.length) {
-    if (left[0] >= right[0]) {
-      sortedArr.push(left.shift());
-    } else {
-      sortedArr.push(right.shift());
-    }
-  }
-  console.log("sortedArr:", sortedArr);
-  return [...sortedArr, ...left, ...right];
+  return result;
 }
-
-function mergeSort(array) {
-  if (array.length < 2) {
-    return array;
-  }
-  const mid = Math.floor(array.length / 2);
-  const left = array.slice(0, mid);
-  const right = array.slice(mid);
-  // console.log(left, mid, right);
-  return merge(mergeSort(left), mergeSort(right));
-}
-
-console.log(mergeSort(arr));
+console.log(
+  solution([
+    [1, 1],
+    [2, 2],
+    [1, 2],
+  ])
+);

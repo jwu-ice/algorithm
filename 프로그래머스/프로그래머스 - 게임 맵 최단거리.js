e4f1434df64b7abfad6baeb4e7edd3d9ac3becfc
jwu-ice <sju02048@naver.com>
2022-07-19 03:08:52 +0900

    Update 프로그래머스 - 게임 맵 최단거리.js

--- 프로그래머스/프로그래머스 - 게임 맵 최단거리.js
+++ 프로그래머스/프로그래머스 - 게임 맵 최단거리.js
@@ -1,4 +1,35 @@
-function solution(maps) {}
+function solution(maps) {
+  const MAP_WIDTH = maps[0].length;
+  const MAP_HEIGHT = maps.length;
+  const visited = [...maps].map((elem) => elem.map(() => 1));
+
+  const direction = [
+    [0, 1],
+    [1, 0],
+    [0, -1],
+    [-1, 0],
+  ];
+
+  const queue = [[0, 0]];
+
+  while (queue.length) {
+    const [y, x] = queue.shift();
+
+    direction.forEach((move) => {
+      const [moveX, moveY] = move;
+      const mx = x + moveX;
+      const my = y + moveY;
+
+      if (mx >= 0 && my >= 0 && mx < MAP_WIDTH && my < MAP_HEIGHT) {
+        if (maps[my][mx] === 1 && visited[my][mx] === 1) {
+          visited[my][mx] = visited[y][x] + 1;
+          queue.push([my, mx]);
+        }
+      }
+    });
+  }
+  return visited[MAP_HEIGHT - 1][MAP_WIDTH - 1] === 1 ? -1 : visited[MAP_HEIGHT - 1][MAP_WIDTH - 1];
+}
 
 solution([
   [1, 0, 1, 1, 1],

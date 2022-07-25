31b2ff66eacfefc2d7b71ab9e8b7c149670455b1
jwu-ice <sju02048@naver.com>
2022-07-26 00:32:08 +0900

    Update 백준1978.js

--- 백준/백준1978.js
+++ 백준/백준1978.js
@@ -2,7 +2,7 @@
 //   .readFileSync("/dev/stdin")
 //   .toString()
 //   .trim()
-//   .split(" ")
+//   .split("\n");
 
 const input = `4
 1 3 5 7`
@@ -10,21 +10,24 @@ const input = `4
   .trim()
   .split("\n");
 
-console.log(input);
-
 const value = input[1].split(" ");
-console.log("value", value);
 
 const result = value.reduce((prev, curr) => {
-  for (let i = 2; i <= Math.floor(curr / 2); i++) {
-    if (curr % i === 0) {
-      return prev;
-    }
-    console.log("curr", curr);
-    return prev + 1;
-  }
-
-  return prev;
+  return isPrime(curr) ? prev + 1 : prev;
 }, 0);
 
-console.log("result", result);
+console.log(result);
+
+function isPrime(number) {
+  if (number < 2) {
+    return false;
+  }
+
+  for (let i = 2; i * i <= number; i++) {
+    if (number % i === 0) {
+      return false;
+    }
+  }
+
+  return true;
+}

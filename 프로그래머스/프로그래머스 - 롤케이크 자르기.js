a9af5bc327b8b9274fd66b91ca0ea0e32031e12a
jwu-ice <sju02048@naver.com>
2022-10-27 18:38:15 +0900

    Create 프로그래머스 - 롤케이크 자르기.js

---
+++ 프로그래머스/프로그래머스 - 롤케이크 자르기.js
@@ -0,0 +1,30 @@
+function solution(topping) {
+  const status = topping.reduce((prev, curr) => {
+    prev[curr] = prev[curr] + 1 || 1;
+    return prev;
+  }, {});
+
+  let chulsoo = new Set();
+  let brotherCount = Object.keys(status).length;
+
+  const result = topping.reduce((prev, curr) => {
+    status[curr]--;
+
+    if (status[curr] <= 0) {
+      brotherCount--;
+    }
+
+    chulsoo.add(curr);
+
+    if (chulsoo.size === brotherCount) {
+      prev++;
+    }
+
+    return prev;
+  }, 0);
+
+  console.log("result", result);
+  return result;
+}
+
+solution([1, 2, 1, 3, 1, 4, 1, 2]); // 2

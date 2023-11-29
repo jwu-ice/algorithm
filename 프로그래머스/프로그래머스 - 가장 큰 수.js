18055754b8725f886b8ca78e3db7b393e8bbe3ae
Jwu <sju02048@naver.com>
2023-11-29 20:04:42 +0900

    프로그래머스 - 가장 큰 수.js

--- 프로그래머스/프로그래머스 - 가장 큰 수.js
+++ 프로그래머스/프로그래머스 - 가장 큰 수.js
@@ -1,21 +1,21 @@
 function solution(numbers) {
-    const getPermutation = (numbers) => {
-        const result = [];
-        if (numbers.length === 1) {
-            return numbers.map((a) => [a]);
-        }
+  const getPermutation = (numbers) => {
+    const result = [];
+    if (numbers.length === 1) {
+      return numbers.map((a) => [a]);
+    }
 
-        numbers.forEach((value, index, arr) => {
-            const rest = [...arr.slice(0, index), ...arr.slice(index + 1)];
-            const permutations = getPermutation(rest);
-            const attached = permutations.map((a) => [value, ...a]);
-            result.push(...attached);
-        });
-        return result;
-    };
+    numbers.forEach((value, index, arr) => {
+      const rest = [...arr.slice(0, index), ...arr.slice(index + 1)];
+      const permutations = getPermutation(rest);
+      const attached = permutations.map((a) => [value, ...a]);
+      result.push(...attached);
+    });
+    return result;
+  };
 
-    const res = getPermutation(numbers).map((a) => a.join(""));
-    return String(res.sort((a, b) => a - b)[res.length - 1]);
+  const res = getPermutation(numbers).map((a) => a.join(""));
+  return String(res.sort((a, b) => a - b)[res.length - 1]);
 }
 // 문자열로 바꾸어 리턴
 solution([3, 30, 34, 5, 9]);
@@ -25,3 +25,24 @@ solution([3, 30, 34, 5, 9]);
 
 // permutation (순열) 알고리즘 사용.
 // https://velog.io/@devjade/JavaScript%EB%A1%9C-%EC%88%9C%EC%97%B4%EA%B3%BC-%EC%A1%B0%ED%95%A9-%EC%95%8C%EA%B3%A0%EB%A6%AC%EC%A6%98-%EA%B5%AC%ED%98%84%ED%95%98%EA%B8%B0
+
+// 다른 방법.
+function solution2(numbers) {
+  numbers.sort((a, b) => {
+    const A = String(a);
+    const B = String(b);
+
+    if (A === B) return 0;
+
+    if (A + B < B + A) return 1;
+
+    return -1;
+  });
+
+  if (numbers.every((v) => v === 0)) {
+    numbers = [0];
+  }
+
+  const result = numbers.join("");
+  return result;
+}
